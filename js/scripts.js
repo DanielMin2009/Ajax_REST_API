cdf40ff3add2cd9e22b56e46c7ff5a0720e06209
DanielMin2009 <daniel.minguella.blasco@gmail.com>
2020-05-13 19:10:53 +0200

    getting jokes

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,17 @@
-$(document).ready(function() {
-    $("#getJokeBtn").click(function() {
-        $.get("http://api.icndb.com/jokes/random", function(data) {
-            $("#answerJoke").html(data.value.joke);
-        });
+const button = $('#getJokeBtn ');
+
+button.click(function () {
+    $.ajax({
+        dataType: 'json',
+        type: "GET",
+        url: 'https://api.icndb.com/jokes/random',
+        success: function (result) {
+            $.each(result, function (index, element) {
+                $("#answerJoke").html(element.joke); // S'assigna el text que es vol.
+            });
+        },
+        error: function () {
+            console.log("Sorry, The API is not working correctly");
+        }
     });
-});
+})
